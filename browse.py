4fdb51f61735df4db9b432688c215d4af841f837
Scrooge <scrooge@Ramis-Air.home>
2023-10-03 00:03:42 +0300

    https support

--- browse.py
+++ browse.py
@@ -1,15 +1,20 @@
 import socket
+import ssl
 
 class URL:
     def __init__(self, url) -> None:
         self.scheme, url = url.split("://", 1)
-        assert self.scheme == "http", \
+        assert self.scheme in ["http", "https"], \
             "Unsupported URL scheme: " + self.scheme
-        
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
         if "/" not in url:
             url = url + "/"
         self.host, url = url.split("/", 1)
         self.path = "/" + url
+
     def request(self):
         s = socket.socket(
             family=socket.AF_INET,
@@ -17,7 +22,11 @@ class URL:
             proto=socket.IPPROTO_TCP,
         )
 
-        s.connect((self.host, 80))
+        s.connect((self.host, self.port))
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+            
         s.send(("GET {} HTTP/1.0\r\n".format(self.path) + \
                 "Host: {}\r\n\r\n".format(self.host)).encode("utf-8"))
         response = s.makefile("r", encoding="utf-8", newline="\r\n")
